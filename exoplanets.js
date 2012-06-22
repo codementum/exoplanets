198a26c31400f316e3114c9d17cf58465a194dfd
Lane Harrison <ltharri1@uncc.edu>
2012-06-21 23:46:42 -0400

    -  added new dataset (exoplanets2.json) -  modified the vis to use radius instead of mass -  added additional data to the left info area

--- exoplanets.js
+++ exoplanets.js
@@ -12,20 +12,18 @@ var r = 960,
 var earthToJupiter = 317.83;
 var jupiterToEarth = 0.00315;
 
-var plutoMass = 0.00218 * jupiterToEarth;
-
-var bubbleSizeScale = 7.647341837;
+var bubbleSizeScale = 1.0;
 
 var ourPlanets = [
-{name:'mercury', mJup:'0.000174'},
-{name:'venus', mJup:'0.00256'},
-{name:'earth', mJup:'0.00315'},
-{name:'mars', mJup:'0.000338'},
-{name:'jupiter', mJup:'1.0'},
-{name:'saturn', mJup:'0.299'},
-{name:'uranus', mJup:'0.0457'},
-{name:'neptune', mJup:'0.054'},
-{name:'pluto', mJup:'0.000006867'},
+{name:'mercury', eRadius:'0.3829'},
+{name:'venus', eRadius:'0.9499'},
+{name:'earth', eRadius:'1.0'},
+{name:'mars', eRadius:'0.533'},
+{name:'jupiter', eRadius:'11.209'},
+{name:'saturn', eRadius:'9.4492'},
+{name:'uranus', eRadius:'4.007'},
+{name:'neptune', eRadius:'3.883'},
+{name:'pluto', eRadius:'0.18'},
 ];
 
 
@@ -36,11 +34,11 @@ var ourPlanets = [
 
 // making a custom scale
 function cFill(val){
-  if(val >= 0.0 && val < 1.0)
+  if(val >= 0.0 && val < 4.02)
     return d3.hsl('#156b87'); // 195 84 53
-  if(val >= 1.0 && val < 8.0)
+  if(val >= 4.02 && val < 11.210)
     return d3.hsl('#876315'); // 41 84 53
-  if(val >= 8.0 && val < 16.0)
+  if(val >= 11.210 && val < 16.0)
     return d3.hsl('#543510'); // 33 81 33 
   else
     return d3.hsl('#872815'); // 10 84 53
@@ -52,58 +50,112 @@ var bubble = d3.layout.pack()
 
 var vis = d3.select("#chart").append("svg")
     .attr("width", r)
-    .attr("height", r)
+    .attr("height", r+100)
     .attr("class", "bubble");
+
+//cb test
+vis.append("text")
+  .attr("id", "colorBlind")
+  .attr("x", r/2)
+  .attr("y", r/2 + 300)
+  .attr("fill", "white")
+  .style("font-size", "750px")
+  .style("text-anchor", "middle")
+  .text("42");
+
 
 // planet title
 vis.append("text")
   .attr("id", "planet")
-  .attr("x", 150)
+  .attr("x", 15)
   .attr("y", 80)
   .style("font-size", "24px")
   .style("font-weight", "bold")
-  .style("text-anchor", "middle")
+  .style("text-anchor", "left")
   .text("Planet Name");
 
 // earth mass info
 vis.append("text")
   .attr("id", "earths")
-  .attr("x", 150)
+  .attr("x", 15)
   .attr("y", 110)
   .style("font-size", "18px")
   .style("font-weight", "bold")
-  .style("text-anchor", "middle")
-  .text("(earth mass)");
+  .style("text-anchor", "left")
+  .text("(earth radius)");
+
+// year info
+vis.append("text")
+  .attr("id", "year")
+  .attr("x", 15)
+  .attr("y", 170)
+  .style("font-size", "18px")
+  .style("font-weight", "bold")
+  .style("text-anchor", "left")
+  .text("year discovered");
+  
+// atmosphere info
+vis.append("text")
+  .attr("id", "atmosphere")
+  .attr("x", 15)
+  .attr("y", 140)
+  .style("font-size", "18px")
+  .style("font-weight", "bold")
+  .style("text-anchor", "left")
+  .text("atmosphere type");
+
+
+
+
+var colorByDistance = false;
+function toggleMetric(){ 
+  if(!colorByDistance){
+    colorByDistance = true;
+    d3.select('#distanceButton').style('fill', cFill(0.5));
+    d3.select('#massButton').style('fill', 'white');
+    updateNodes();
+  } else {
+    colorByDistance = false;
+    d3.select('#massButton').style('fill', cFill(0.5));
+    d3.select('#distanceButton').style('fill', 'white');
+    updateNodes();
+  }
+}
 
 // earth mass info
-var colorByDistance = false;
 vis.append("rect")
-  .attr("id", "colorButton")
-  .attr("x", r/2)
-  .attr("y", 20)
+  .attr("id", "massButton")
+  .attr("x", r/2 - 50)
+  .attr("y", 10)
+  .attr("width", 10)
+  .attr("height", 10)
+  .style("stroke", 'black')
+  .style("fill", cFill(0.5))
+  .on("click", toggleMetric);
+
+vis.append("rect")
+  .attr("id", "distanceButton")
+  .attr("x", r/2 - 50)
+  .attr("y", 30)
   .attr("width", 10)
   .attr("height", 10)
   .style("stroke", 'black')
   .style("fill", 'white')
-  .on("click", function(){
-    console.log('click');
-    d3.select(this)
-      .transition()
-      .duration(500)
-      .style("fill", function() {
-        if(!colorByDistance){
-          colorByDistance = true;
-          d3.select('#colorButtonText').text('color by size');
-          updateNodes();
-          return cFill(0.5);
-        } else {
-          colorByDistance = false;
-          d3.select('#colorButtonText').text('color by distance');
-          updateNodes();
-          return 'white';
-        }
-      })
-  });
+  .on("click", toggleMetric);
+
+vis.append("text")
+  .attr("id", "distanceButtonText")
+  .attr("x", r/2+15-50)
+  .attr("y", 19)
+  .text("color by radius");
+
+vis.append("text")
+  .attr("id", "distanceButtonText")
+  .attr("x", r/2+15-50)
+  .attr("y", 39)
+  .text("color by distance to nearest star");
+
+
 
 var updateNodes = function() {
   var circs = vis.selectAll("g.node").selectAll('circle');
@@ -121,10 +173,14 @@ var updateNodes = function() {
       if(colorByDistance){
         d3.select("#planet").text(d.className);
         d3.select("#earths").text("("+format(d.distance)+" * earth-to-sun distance)");
+        d3.select("#year").text("year discovered: "+d.year);
+        d3.select("#atmosphere").text("atmosphere type: "+d.atmosphere);
         d3.select(this).style("fill", function(d) { return d3.hsl(dFill(d.distance)).darker(); });
     } else {
         d3.select("#planet").text(d.className);
-        d3.select("#earths").text("("+format(d.value*earthToJupiter)+" earth mass)");
+        d3.select("#earths").text("("+format(d.value)+" *  earth-radius)");
+        d3.select("#year").text("year discovered: "+d.year);
+        d3.select("#atmosphere").text("atmosphere type: "+d.atmosphere);
         d3.select(this).style("fill", function(d) { return cFill(d.value).brighter(); });
     }
     })
@@ -137,12 +193,7 @@ var updateNodes = function() {
     });
 }
 
-// earth mass info
-vis.append("text")
-  .attr("id", "colorButtonText")
-  .attr("x", r/2+15)
-  .attr("y", 29)
-  .text("color by distance");
+
 
 // other planets
 vis.selectAll('circle.ourPlanets')
@@ -151,20 +202,22 @@ vis.selectAll('circle.ourPlanets')
   .attr('class', 'ourPlanets')
   .attr('id', function(d) { return d.name; })
   .attr('cx', function(d, i) { return r - 70; })
-  .attr('cy', function(d, i) { return 80 + 20*i; })
+  .attr('cy', function(d, i) { return 80 + 25*i; })
   .attr('r', function(d, i) { 
-    return (1 + d.mJup*bubbleSizeScale); 
+    return d.eRadius*bubbleSizeScale; 
   })
   .style('fill', function(d, i) { 
-      return cFill(d.mJup); 
+      return cFill(d.eRadius); 
   })
   .on("mouseover", function(d) {
     d3.select("#planet").text(d.name);
-    d3.select("#earths").text("("+format(d.mJup*earthToJupiter)+" earth mass)");
-    d3.select(this).style("fill", function(d) { return cFill(d.mJup).brighter(); });
+    d3.select("#earths").text("("+format(d.eRadius)+" * earth radius)");
+    d3.select("#year").text("");
+    d3.select("#atmosphere").text("");
+    d3.select(this).style("fill", function(d) { return cFill(d.eRadius).brighter(); });
   })
   .on("mouseout", function(d) {
-    d3.select(this).style("fill", function(d) { return cFill(d.mJup); });
+    d3.select(this).style("fill", function(d) { return cFill(d.eRadius); });
   });
   
 vis.selectAll('text.ourPlanets')
@@ -172,7 +225,7 @@ vis.selectAll('text.ourPlanets')
   .enter().append('text')
   .attr('class', 'ourPlanets')
   .attr('x', function(d, i) { return r - 50; })
-  .attr('y', function(d, i) { return 80 + 20*i; })
+  .attr('y', function(d, i) { return 80 + 26*i; })
   .style("font-size", "12px")
   .text(function(d) { return d.name; })
   .on('mouseover', function(d, i){
@@ -184,28 +237,30 @@ vis.selectAll('text.ourPlanets')
   .on('mouseout', function(d, i){
     if(d.name === 'pluto'){
       d3.select(this).text(d.name);
-      d3.selectAll('circle.ourPlanets').select(d.name).style('fill', cFill(d.mJup));
+      d3.selectAll('circle.ourPlanets').select(d.name).style('fill', cFill(d.eRadius));
     }
   });
 
 
-d3.json("exoplanets.json", function(json) {
+d3.json("exoplanets2.json", function(json) {
   var node = vis.selectAll("g.node")
       .data(bubble.nodes(classes(json))
       .filter(function(d) { return !d.children; }))
     .enter().append("g")
       .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+      .attr("transform", function(d) { return "translate(" + d.x + "," + (80 + d.y) + ")"; });
 
   node.append("title")
-      .text(function(d) { return d.className + "\n Jupiter Mass: " + d.value + "\n AU (distance): " + d.distance; });
+      .text(function(d) { return d.className + "\n * Earth Radius: " + d.value + "\n AU (distance to star): " + d.distance; });
 
   node.append("circle")
       .attr("r", function(d) { return d.r; })
       .style("fill", function(d) { return cFill(d.value); })
       .on("mouseover", function(d) {
         d3.select("#planet").text(d.className);
-        d3.select("#earths").text("("+format(d.value*earthToJupiter)+" earth mass)");
+        d3.select("#earths").text("("+format(d.value)+" * earth radius)");
+        d3.select("#year").text("year discovered: "+d.year);
+        d3.select("#atmosphere").text("atmosphere type: "+d.atmosphere);
         d3.select(this).style("fill", function(d) { return cFill(d.value).brighter(); });
       })
       .on("mouseout", function(d) {
@@ -226,10 +281,45 @@ function classes(root) {
 
   function recurse(name, node) {
     if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
-    else classes.push({packageName: name, className: node.Planet_Name, value: node.Pl_Mass, distance: node.Pl_semiaxis});
+    else classes.push({packageName: name, className: node.P_Name, value: node.P_Radius__EU, distance: node.P_Mean_Distance__AU, year: node.P_Disc_Year, atmosphere: node.P_Atmosphere_Class});
   }
 
   recurse(null, root);
   return {children: classes};
 }
 
+function colorBlind(){
+  // switch to red
+//  $('#distanceButton').click();
+
+  var circs = vis.selectAll("g.node").selectAll('circle');
+  circs.transition()
+    .duration(1000)
+    .style("fill-opacity", ".7");
+
+  // cb text
+  vis.transition()
+    .duration(1000)
+    .select("text#colorBlind")
+    .attr("fill", "#a9a9a9")
+    .style("fill-opacity", ".7");
+}
+
+
+(function (){
+
+"use strict";
+
+// test that it loads locally with ender.js and globally with vanilla konami.js
+var konami = new Konami();
+
+konami.code = function() {
+  colorBlind();  
+};
+
+konami.iphone.code = function() {
+  colorBlind();  
+};
+
+konami.load()
+}());
