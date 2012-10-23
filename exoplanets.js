9b695ece82ae8f7ca8314f6ed1fda4fcf8624655
Lane Harrison <ltharri1@uncc.edu>
2012-10-23 14:49:58 -0400

    new planets and cleanup

--- exoplanets.js
+++ exoplanets.js
@@ -13,15 +13,15 @@ var earthToJupiter = 317.83;
 var jupiterToEarth = 0.00315;
 
 var ourPlanets = [
-{name:'mercury', eRadius:'0.3829'},
-{name:'venus', eRadius:'0.9499'},
-{name:'earth', eRadius:'1.0'},
-{name:'mars', eRadius:'0.533'},
-{name:'jupiter', eRadius:'11.209'},
-{name:'saturn', eRadius:'9.4492'},
-{name:'uranus', eRadius:'4.007'},
-{name:'neptune', eRadius:'3.883'},
-{name:'pluto', eRadius:'0.18'},
+{name:'mercury', radius:'0.3829'},
+{name:'venus', radius:'0.9499'},
+{name:'earth', radius:'1.0'},
+{name:'mars', radius:'0.533'},
+{name:'jupiter', radius:'11.209'},
+{name:'saturn', radius:'9.4492'},
+{name:'uranus', radius:'4.007'},
+{name:'neptune', radius:'3.883'},
+{name:'pluto', radius:'0.18'},
 ];
 
 
@@ -31,7 +31,16 @@ var ourPlanets = [
         .range([d3.hsl('#DCDCDC'), d3.hsl('#156b87')]);
 
 // making a custom scale
-function cFill(val){
+function cFill(d){
+  // TODO earth sized so too small to see!
+  if(d.name === 'alf Cen B b')
+    return d3.hsl('#E0E35D');
+
+  // year check
+  if(d.year === '2012')
+    return d3.hsl('#496B28');
+
+  var val = d.radius;
   if(val >= 0.0 && val < 4.02)
     return d3.hsl('#156b87'); // 195 84 53
   if(val >= 4.02 && val < 11.210)
@@ -162,7 +171,7 @@ var updateNodes = function() {
     .duration(1000)
     .style("fill", function(d) { 
       if(!colorByDistance)
-        return cFill(d.radius); 
+        return cFill(d); 
       else
         return dFill(d.distance);
     });
@@ -180,14 +189,14 @@ var updateNodes = function() {
         d3.select("#earths").text("("+format(d.radius)+" *  earth-radius)");
         d3.select("#year").text("year discovered: "+d.year);
         d3.select("#atmosphere").text("atmosphere type: "+d.atmosphere);
-        d3.select(this).style("fill", function(d) { return cFill(d.radius).brighter(); });
+        d3.select(this).style("fill", function(d) { return cFill(d).brighter(); });
     }
     })
     .on("mouseout", function(d) {
       if(colorByDistance){
         d3.select(this).style("fill", function(d) { return d3.hsl(dFill(d.distance)); });
       } else {
-          d3.select(this).style("fill", function(d) { return cFill(d.radius); });
+          d3.select(this).style("fill", function(d) { return cFill(d); });
       }
     });
 }
@@ -202,20 +211,20 @@ vis.selectAll('circle.ourPlanets')
   .attr('cx', function(d, i) { return r - 70; })
   .attr('cy', function(d, i) { return 80 + 25*i; })
   .attr('r', function(d, i) { 
-    return d.eRadius; 
+    return d.radius; 
   })
   .style('fill', function(d, i) { 
-      return cFill(d.eRadius); 
+      return cFill(d); 
   })
   .on("mouseover", function(d) {
     d3.select("#planet").text(d.name);
-    d3.select("#earths").text("("+format(d.eRadius)+" * earth radius)");
+    d3.select("#earths").text("("+format(d.radius)+" * earth radius)");
     d3.select("#year").text("");
     d3.select("#atmosphere").text("");
-    d3.select(this).style("fill", function(d) { return cFill(d.eRadius).brighter(); });
+    d3.select(this).style("fill", function(d) { return cFill(d).brighter(); });
   })
   .on("mouseout", function(d) {
-    d3.select(this).style("fill", function(d) { return cFill(d.eRadius); });
+    d3.select(this).style("fill", function(d) { return cFill(d); });
   });
   
 vis.selectAll('text.ourPlanets')
@@ -228,10 +237,10 @@ vis.selectAll('text.ourPlanets')
   .text(function(d) { return d.name; })
   .on("mouseover", function(d) {
     d3.select("#planet").text(d.name);
-    d3.select("#earths").text("("+format(d.eRadius)+" * earth radius)");
+    d3.select("#earths").text("("+format(d.radius)+" * earth radius)");
     d3.select("#year").text("");
     d3.select("#atmosphere").text("");
-    d3.select('#'+d.name).style("fill", function(d) { return cFill(d.eRadius).brighter(); });
+    d3.select('#'+d.name).style("fill", function(d) { return cFill(d).brighter(); });
 
     if(d.name === 'pluto'){
       d3.select(this).text("jk!");
@@ -239,14 +248,14 @@ vis.selectAll('text.ourPlanets')
     }
   })
   .on("mouseout", function(d) {
-    d3.select('#'+d.name).style("fill", function(d) { return cFill(d.eRadius); });
+    d3.select('#'+d.name).style("fill", function(d) { return cFill(d); });
     if(d.name === 'pluto'){
       d3.select(this).text(d.name);
-      d3.selectAll('circle.ourPlanets').select(d.name).style('fill', cFill(d.eRadius));
+      d3.selectAll('circle.ourPlanets').select(d.name).style('fill', cFill(d));
     }
   });
  
-d3.json("exoplanets2.json", function(json) {
+d3.json("exoplanets_oct2012.json", function(json) {
   var node = vis.selectAll("g.node")
       .data(bubble.nodes(classes(json))
       .filter(function(d) { return !d.children; }))
@@ -258,17 +267,17 @@ d3.json("exoplanets2.json", function(json) {
       .text(function(d) { return d.className + "\n * Earth Radius: " + d.radius + "\n AU (distance to star): " + d.distance; });
 
   node.append("circle")
-      .attr("r", function(d) {  return d.r; })
-      .style("fill", function(d) { return cFill(d.radius); })
+      .attr("r", function(d) { return Math.sqrt(d.r * d.r * .8); })
+      .style("fill", function(d) { return cFill(d); })
       .on("mouseover", function(d) {
         d3.select("#planet").text(d.className);
-        d3.select("#earths").text("("+format(d.radius)+" * earth radius)");
+        d3.select("#earths").text("("+format(d)+" * earth radius)");
         d3.select("#year").text("year discovered: "+d.year);
         d3.select("#atmosphere").text("atmosphere type: "+d.atmosphere);
-        d3.select(this).style("fill", function(d) { return cFill(d.radius).brighter(); });
+        d3.select(this).style("fill", function(d) { return cFill(d).brighter(); });
       })
       .on("mouseout", function(d) {
-        d3.select(this).style("fill", function(d) { return cFill(d.radius); });
+        d3.select(this).style("fill", function(d) { return cFill(d); });
       });
 
       // names on circles
@@ -285,7 +294,8 @@ function classes(root) {
 
   function recurse(name, node) {
     if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
-    else classes.push({packageName: name, className: node.P_Name, radius: node.P_Radius__EU, distance: node.P_Mean_Distance__AU, year: node.P_Disc_Year, atmosphere: node.P_Atmosphere_Class});
+    else classes.push({packageName: name, className: node.P_Name, radius: node.P_Radius_EU, distance: node.P_Mean_Distance_AU, year: node.P_Disc_Year, atmosphere: node.P_Atmosphere_Class});
+
   }
 
   recurse(null, root);
